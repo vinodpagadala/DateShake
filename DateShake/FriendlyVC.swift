e7f01143989ec6de38060e6ed5ac6685d25b3a14
Manoj Muthireddy <Manoj@cpe-2607-fcc8-9b0a-7800-42c-73f-7060-335d.dyn6.twc.com>
2017-12-27 21:03:03 -0500

    friendlyVC

---
+++ DateShake/FriendlyVC.swift
@@ -0,0 +1,25 @@
+//
+//  FriendlyVC.swift
+//  DateShake
+//
+//  Created by Manoj Muthireddy on 12/27/17.
+//  Copyright © 2017 Manoj Muthireddy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FriendlyVC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool)  {
+        super.viewDidAppear(animated)
+        // Do any additional setup after loading the view, typically from a nib.
+        //gifImage.loadGif(name: "shakeblue1")
+        
+    }
+}
